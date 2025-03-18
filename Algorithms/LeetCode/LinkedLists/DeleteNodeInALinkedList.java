33b07aacb976a2b521c53fae27b1004826affa7e
utakarsh23 <utakarsh.t@somaiya.edu>
2025-03-18 22:32:18 +0530

    huhh

--- Algorithms/LeetCode/LinkedLists/DeleteNodeInALinkedList.java
+++ Algorithms/LeetCode/LinkedLists/DeleteNodeInALinkedList.java
@@ -1,6 +1,9 @@
 package LeetCode.LinkedLists;
 
 public class DeleteNodeInALinkedList {
+    public static void main(String[] args) {
+
+    }
     public void deleteNode(ListNode node) {
         node.val = node.next.val;
         node.next = node.next.next;
