92717bb4c9a7a32010b85212a1ffa6a3bbf90050
utakarsh23 <utakarsh.t@somaiya.edu>
2025-04-01 01:08:20 +0530

    huh

--- Algorithms/GFG/rotateArray.java
+++ Algorithms/GFG/rotateArray.java
@@ -6,7 +6,7 @@ public class rotateArray {
     public static void main(String[] args) {
         int[] arr = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
         int d = 3;
-        rotateArrays(arr, d);
+        rotateArrays1(arr, d);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -25,4 +25,22 @@ public class rotateArray {
             arr[j++] = dumArr[i++];
         }
     }
+
+    //m2
+    static void rotateArrays1(int[] arr, int d) {
+        reverse(arr, 0, arr.length-1-d);
+        reverse(arr, arr.length-d, arr.length-1);
+        reverse(arr, 0, arr.length-1);
+    }
+
+    static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
 }
---
+++ Algorithms/LeetCode/nextPermutation.java
@@ -0,0 +1,81 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class nextPermutation {
+    public static void main(String[] args) {
+        int[] arr = {3,2,1};
+        nextPermutation(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    //works but not the optimalest approach
+    static void nextPermutation(int[] arr) {
+        // code here
+        int i = arr.length-2;
+        int ind = -1;
+        while (i >= 0) {
+            if(arr[i] < arr[i+1]){
+                ind  = i;
+                break;
+            }
+            i--;
+        }
+        if(ind == -1) {
+            Arrays.sort(arr);
+            return;
+        }
+
+        int min = Integer.MAX_VALUE;
+        int j = arr.length-1;
+        int sInd = 0;
+        while (j > i) {
+            if(arr[j] > arr[i]) {
+                int temp = min;
+                min = Math.min(min, arr[j]);
+                if(min != temp) sInd = j;
+
+            }
+            j--;
+        }
+        swap(arr, i, sInd);
+        sortPartially(arr, i+1);
+    }
+
+    static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    static void sortPartially(int[] arr, int fromInd) {
+        int[] li = Arrays.copyOfRange(arr, fromInd, arr.length);
+        int j = 0;
+        Arrays.sort(li);
+        for (int i = fromInd; i < arr.length; i++) {
+            arr[i] = li[j++];
+        }
+    }
+
+
+    //m2
+    public void nextPermutation1(int[] arr) {
+        int i = arr.length-2;
+        while (i > -1 && arr[i] >= arr[i+1]) {
+            i--;
+        }
+        if(i == -1) reverse(arr, 0, arr.length);
+        int j = arr.length-1;
+        while (arr[i] > arr[j]) {
+            j--;
+        }
+        swap(arr, i, j);
+        reverse(arr, i+1, arr.length-1);
+    }
+
+    public void reverse(int[] arr, int a, int b) {
+        while (a < b) {
+            swap(arr, a++, b--);
+        }
+    }
+}
