0b5b18fdbf4900773137fea9fc943ce4302a7127
utakarsh23 <utakarsh.t@somaiya.edu>
2025-04-04 22:43:52 +0530

    g

---
+++ Algorithms/GFG/StockBuyAndSellII.java
@@ -0,0 +1,19 @@
+package GFG;
+
+public class StockBuyAndSellII {
+    public int maximumProfit(int prices[]) {
+        // code here
+        int st = 0, ans = 0, end = 0;
+        for(int i = 0; i < prices.length; i++) {
+            if(prices[i] > prices[end]) {
+                end = i;
+            } else {
+                ans += prices[end]-prices[st];
+                st = i;
+                end = i;
+            }
+        }
+        ans += prices[end]-prices[st];
+        return ans;
+    }
+}
