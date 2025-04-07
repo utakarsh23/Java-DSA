cffd90defb32ce1d5e09cbc38e9233d6732f384f
utakarsh23 <utakarsh.t@somaiya.edu>
2025-04-07 20:25:43 +0530

    g

---
+++ Algorithms/GFG/KNPAlgo.java
@@ -0,0 +1,25 @@
+package GFG;
+
+public class KNPAlgo {
+    public static void main(String[] args) {
+        String s1 = "aaaaaaaaaaaab";
+        String s2 = "aaaab";
+        System.out.println(knpAlgo(s1, s2));
+    }
+    static int knpAlgo(String s1, String s2) {
+        int i = 0;
+        int j = 0;
+        int temp = 0;
+        while(i < s1.length()) {
+            temp++;
+            while (j < s2.length() && s1.charAt(i) == s2.charAt(j)) {
+                i++;
+                j++;
+            }
+            if(j == s2.length()) return i-j;
+            i = temp;
+            j = 0;
+        }
+        return -1;
+    }
+}
---
+++ Algorithms/LeetCode/kanadesAlgorithm.java
@@ -0,0 +1,21 @@
+package LeetCode;
+
+public class kanadesAlgorithm {
+
+    int maxSubarraySum(int[] arr) {
+        // Your code here
+        int sum = 0;
+        int maxi = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length; i++){
+
+            sum += arr[i];
+            if(sum > maxi) {
+                maxi = sum;
+            }
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return maxi;
+    }
+}
