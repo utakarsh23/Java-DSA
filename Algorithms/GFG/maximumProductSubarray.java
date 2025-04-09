41972ac60c0121a875c8cb8a0dfc22e3b202bc3f
utakarsh23 <utakarsh.t@somaiya.edu>
2025-04-09 19:06:36 +0530

    g

--- Algorithms/GFG/maximumProductSubarray.java
+++ Algorithms/GFG/maximumProductSubarray.java
@@ -2,9 +2,15 @@ package GFG;
 
 class maximumProductSubarray {
     public static void main(String[] args) {
-
+        int[] nums = {2,3,-2,4};
+//        System.out.println(maxProduct2(nums));
+        int[] arr = {1, 2, 3, 4, 4, 5, 6, 6, 6, 7, 8, 8, 9};
+        int id = 3;
+        for(int i = 0; i < arr.length && arr[i] != id; i++) {
+            System.out.println(arr[i]);
+        }
     }
-    public int maxProduct(int[] nums) {
+    static int maxProduct(int[] nums) {
         int maxProduct = nums[0];
         int minProduct = nums[0];
         int ans = nums[0];
@@ -23,7 +29,7 @@ class maximumProductSubarray {
     //
 
     //correct
-    public int maxProduct1(int[] nums) {
+    static int maxProduct1(int[] nums) {
         int res = Integer.MIN_VALUE;
         for (int n : nums) {
             res = Math.max(res, n);
@@ -40,5 +46,20 @@ class maximumProductSubarray {
         }
 
         return res;
+    }
+
+    static int maxProduct2(int[] nums) {
+        int pre = 1, suff = 1;
+        int max = Integer.MIN_VALUE;
+        int n = nums.length;
+        for(int i = 0; i < n; i++) {
+            if(suff == 0) suff = 1;
+            if(pre == 0) pre = 1;
+            pre = nums[i] * pre;
+            suff = nums[n-i-1] * suff;
+            max = Math.max(max, Math.max(pre, suff));
+        }
+        return max;
+
     }
 }
