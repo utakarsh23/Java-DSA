ef5c500d8b38650e06ec540bb7bb71cfd1276e03
utakarsh23 <utakarsh.t@somaiya.edu>
2025-04-18 23:27:45 +0530

    h

--- Algorithms/GFG/StringROtations.java
+++ Algorithms/GFG/StringROtations.java
@@ -1,4 +1,31 @@
 package GFG;
 
+import java.util.ArrayList;
+
 public class StringROtations {
+    public static void main(String[] args) {
+        String s1 = "abcd";
+        String s2 = "bcda";
+        System.out.println(areRotations1(s1, s2));
+
+    }
+    static boolean areRotations(String s1, String s2) {
+        ArrayList<String> li = new ArrayList<>(s1.length());
+        String temp = s2;
+        for (int i = 0; i < s1.length(); i++) {
+            s2 = s2.charAt(s2.length()-1)+s2.substring(0, s2.length()-1);
+            temp = temp.substring(1, s2.length())+temp.charAt(0);
+
+            if(li.contains(temp) || li.contains(s2)) {
+                return true;
+            }
+            li.add(s2);
+            li.add(temp);
+        }
+        return false;
+    }
+    static boolean areRotations1(String s1, String s2) {
+        String t = s1 + s1;
+        return (t).contains(s2);
+    }
 }
--- Algorithms/GFG/areRotationss.java
+++ Algorithms/GFG/areRotationss.java
@@ -1,39 +1,81 @@
 package GFG;
 
 class areRotationss {
+    public static void main(String[] args) {
+        System.out.println(areRotations("abcd", "cdab"));
+    }
     // Function to check if two strings are rotations of each other or not.
     public static boolean areRotations(String s1, String s2) {
         String comb = s1 + s1;
-        return search(comb, s2);
+        boolean s = search(s2, comb);
+        return s;
     }
-    public static boolean search(String pat, String txt) {
-        int[] lps = computeLPSArray(pat);
-        int i= 0, j = 0;
-        while(i < txt.length()) {
-            if(pat.charAt(j) == txt.charAt(i)) {
-                i++;
-                j++;
-                if(j == pat.length()) return true;
+//    public static boolean search(String pat, String txt) {
+//        int[] lps = computeLPSArray(pat);
+//        int i= 0, j = 0;
+//        while(i < txt.length()) {
+//            if(pat.charAt(j) == txt.charAt(i)) {
+//                i++;
+//                j++;
+//                if(j == pat.length()) return true;
+//            } else {
+//                if(j != 0) {
+//                    j = lps[j - 1];
+//                } else {
+//                    i++;
+//                }
+//            }
+//        }
+//        return false;
+//    }
+//    public static int[] computeLPSArray(String pattern) {
+//        int[] lps = new int[pattern.length()];
+//        int len = 0, i = 1;
+//        while(i < pattern.length()) {
+//            if(pattern.charAt(i) == pattern.charAt(len)) {
+//                len++;
+//                lps[i++] = len;
+//            } else {
+//                if(len != 0) {
+//                    len = lps[len -1];
+//                } else {
+//                    lps[i++] = 0;
+//                }
+//            }
+//        }
+//        return lps;
+//    }
+public static boolean search(String pat, String txt) {
+    int[] lps = computeLPSArray(pat);
+    int i = 0, j = 0;
+
+    while (i < txt.length()) {
+        if (pat.charAt(j) == txt.charAt(i)) {
+            i++;
+            j++;
+            if (j == pat.length()) return true;
+        } else {
+            if (j != 0) {
+                j = lps[j - 1];
             } else {
-                if(j != 0) {
-                    j = lps[j - 1];
-                } else {
-                    i++;
-                }
+                i++;
             }
         }
-        return false;
     }
+    return false;
+}
+
     public static int[] computeLPSArray(String pattern) {
         int[] lps = new int[pattern.length()];
         int len = 0, i = 1;
-        while(i < pattern.length()) {
-            if(pattern.charAt(i) == pattern.charAt(len)) {
+
+        while (i < pattern.length()) {
+            if (pattern.charAt(i) == pattern.charAt(len)) {
                 len++;
                 lps[i++] = len;
             } else {
-                if(len != 0) {
-                    len = lps[len -1];
+                if (len != 0) {
+                    len = lps[len - 1];
                 } else {
                     lps[i++] = 0;
                 }
@@ -41,4 +83,5 @@ class areRotationss {
         }
         return lps;
     }
+
 }
