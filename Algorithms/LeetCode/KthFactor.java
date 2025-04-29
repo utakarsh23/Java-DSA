f85f94ff28024126be0a8cacfb85c7be0e271849
utakarsh23 <utakarsh.t@somaiya.edu>
2025-04-29 19:26:33 +0530

    h

---
+++ Algorithms/LeetCode/KthFactor.java
@@ -0,0 +1,19 @@
+package LeetCode;
+
+public class KthFactor {
+    public static void main(String[] args) {
+        System.out.println(kthFactor(12, 3));
+    }
+    static int kthFactor(int n, int k) {
+        // ArrayList<Integer> li = new ArrayList<>();
+        for(int i = 1; i <= n; i++) {
+            int s = n % i;
+            if(s == 0) {
+                k--;
+                if(k == 0)
+                return i;
+            }
+        }
+        return -1;
+    }
+}
