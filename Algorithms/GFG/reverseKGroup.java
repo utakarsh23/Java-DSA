8ae38751a2463ce52706316208c1a91d47f923b5
utakarsh23 <utakarsh.t@somaiya.edu>
2025-06-04 18:53:34 +0530

    i

---
+++ Algorithms/GFG/reverseKGroup.java
@@ -0,0 +1,40 @@
+package GFG;
+
+import LeetCode.LinkedLists.ListNode;
+
+class reverseKGroup {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || k == 1) return head;
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prevGroupEnd = dummy;
+        ListNode curr = head;
+
+        while (true) {
+            ListNode check = curr;
+            int count = 0;
+            while (check != null && count < k) {
+                check = check.next;
+                count++;
+            }
+            if (count < k) break; 
+
+            ListNode prev = null;
+            ListNode tail = curr;
+            for (int i = 0; i < k; i++) {
+                ListNode nextNode = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = nextNode;
+            }
+
+            prevGroupEnd.next = prev;
+            tail.next = curr;
+            prevGroupEnd = tail;
+        }
+
+        return dummy.next;
+    }
+}
