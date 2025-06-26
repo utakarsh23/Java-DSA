5e153fef27503356da5f4ef43bfc3cd30c31d0e4
utakarsh23 <utakarsh.t@somaiya.edu>
2025-06-26 23:23:57 +0530

    no major changes

--- Algorithms/LeetCode/PrimePalindromes.java
+++ Algorithms/LeetCode/PrimePalindromes.java
@@ -2,8 +2,10 @@ package LeetCode;
 
 public class PrimePalindromes {
     public static void main(String[] args) {
-        System.out.println(primePalindrome(9989900));
+        System.out.println(primePalindrime(8));
     }
+
+    //brute force
     static int primePalindrome(int n) {
         int i = n;
         while(true) {
@@ -29,5 +31,25 @@ public class PrimePalindromes {
         String sb = new StringBuilder(s).reverse().toString();
         return s.equals(sb);
 
+    }
+
+    static int primePalindrime(int n) {
+        if (n <= 2) return 2;
+        for (int i = n; i < 12; i++) {
+            if (isPalin(i) && isPrime(i)) return i;
+        }
+        for (int len = 1; len < 6; len++) {
+
+            for (int root = (int)Math.pow(10, len - 1); root < (int)Math.pow(10, len); root++) {
+                String left = Integer.toString(root);
+                String right = new StringBuilder(left.substring(0, left.length() - 1)).reverse().toString();
+                int cand = Integer.parseInt(left + right);
+                if (cand >= n && isPrime(cand)) {
+                    return cand;
+                }
+            }
+        }
+
+        return 100030001;
     }
 }
