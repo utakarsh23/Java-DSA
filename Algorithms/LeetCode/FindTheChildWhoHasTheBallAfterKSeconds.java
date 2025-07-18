f55a9950a3a95786fa8f802565a1078f0ad194ff
utakarsh23 <utakarsh.t@somaiya.edu>
2025-07-18 19:25:24 +0530

    huh

---
+++ Algorithms/LeetCode/FindTheChildWhoHasTheBallAfterKSeconds.java
@@ -0,0 +1,26 @@
+package LeetCode;
+
+public class passThePillow {
+    public static void main(String[] args) {
+        System.out.println(passThePillow(4, 5));
+    }
+    static public int passThePillow(int n, int time) {
+        int bp1 = 1;
+        boolean fwd = true;
+        while (time != 0) {
+            if (fwd) {
+                bp1++;
+                if (bp1 == n) {
+                    fwd = false;
+                }
+            } else {
+                bp1--;
+                if (bp1 == 1) {
+                    fwd = true;
+                }
+            }
+            time--;
+        }
+        return bp1;
+    }
+}
---
+++ Algorithms/LeetCode/passThePillow.java
@@ -0,0 +1,7 @@
+package LeetCode;
+
+public class newQuestion {
+    public static void main(String[] args) {
+        System.out.println("heyyyyy");
+    }
+}
