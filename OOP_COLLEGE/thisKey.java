46d1b37973afb69efd48b00272f11ccd0b52a886
utakarsh23 <utakarsh.t@somaiya.edu>
2025-07-31 13:28:30 +0530

    h

--- OOP_COLLEGE/thisKey.java
+++ OOP_COLLEGE/thisKey.java
@@ -1,17 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class thisKey {
     public static void main(String[] args) {
-        aps aps = new aps(100);
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 7;
+        //n^2
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if(arr[i] + arr[j] == target) {
+                    System.out.println(i + " " + j);
+                }
+            }
+        }
     }
-}
 
-class aps {
-    aps() {
-        System.out.println("Ayyyyy");
-    }
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
 
-    aps(int a) {
-        this();
-        System.out.println(a);
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+
+        return new int[]{};
     }
 }
 
