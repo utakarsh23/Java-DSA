610cc097ff4376312f67c6851b0c60c2495d5636
utakarsh23 <utakarsh.t@somaiya.edu>
2025-08-11 00:15:59 +0530

    kk

---
+++ Algorithms/LeetCode/MaximumIntersectionToBalenceAParanthesisString.java
@@ -0,0 +1,36 @@
+package LeetCode;
+
+import java.util.Stack;
+
+public class MaximumIntersectionToBalenceAParanthesisString {
+    public static void main(String[] args) {
+
+    }
+    static public int minInsertions(String s) {
+        Stack<Character> stack=new Stack<>();
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c == '('){
+                stack.push(c);
+            }
+            else{
+                if(i+1<s.length()&& s.charAt(i+1)==')'){
+                    i++;
+                }
+                else {
+                    count++;
+                }
+
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }
+                else{
+                    count++;
+                }
+            }
+        }
+        count += 2*stack.size();
+        return count;
+    }
+}
