db2ba1763fea17b0403c5705f2a16955f196469d
utakarsh23 <utakarsh.t@somaiya.edu>
2025-09-14 19:30:40 +0530

    -trees yayyyyy

--- Algorithms/LeetCode/Trees/PathSum.java
+++ Algorithms/LeetCode/Trees/PathSum.java
@@ -1,6 +1,9 @@
 package LeetCode.Trees;
 
 public class PathSum {
+    public static void main(String[] args) {
+
+    }
     public boolean hasPathSum(TreeNode root, int targetSum) {
         return helper(root, targetSum);
     }
