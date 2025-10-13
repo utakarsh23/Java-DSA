106789bf0cc595361569146ec16bc33396a2efe3
utakarsh23 <utakarsh.t@somaiya.edu>
2025-10-13 23:55:53 +0530

    huh

--- Algorithms/LeetCode/TwoOutOfThree.java
+++ Algorithms/LeetCode/TwoOutOfThree.java
@@ -1,5 +1,8 @@
 package LeetCode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class TwoOutOfThree {
     public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
         var res=new ArrayList<Integer>();
@@ -14,4 +17,24 @@ class TwoOutOfThree {
 
         return res;
     }
+
+    public List<Integer> twoOutOfThree1(int[] nums1, int[] nums2, int[] nums3) {
+        var res=new ArrayList<Integer>();
+        int [] index1=new int[101];
+
+
+        for(int i:nums1) index1[i]=1;
+        for(int i:nums1) {
+            if(index1[i]==1) index1[i]=2;
+            else index1[i]=1;
+        }
+        for(int i:nums1) {
+            if(index1[i]==1) index1[i]=2;
+        }
+
+        for(int i=0;i<101;i++) if(index1[i]>1) res.add(i);
+
+        return res;
+    }
+
 }
