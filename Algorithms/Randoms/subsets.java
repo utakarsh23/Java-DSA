27cea0fa76be64b40f7d82e91ad1ba6d31f52340
utakarsh23 <utakarsh.t@somaiya.edu>
2025-10-27 22:24:09 +0530

    --

--- Algorithms/Randoms/subsets.java
+++ Algorithms/Randoms/subsets.java
@@ -1,12 +1,31 @@
 package Randoms;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class subsets {
     public static void main(String[] args) {
-
+        int[] ints = {1, 2, 3, 5};
+        int[] proArr = new int[4];
+        System.out.println(subs(proArr, ints));
     }
 
 
-    static int[] subs(int[] arr) {
+    static List<String> subs(int[] proArr, int[] arr) {
+        ArrayList<String> result = new ArrayList<>();
+        if(arr.length == 0) {
+            result.add(Arrays.toString(proArr));
+            return result;
+        }
+        int first = arr[0];
+        int rest[] = Arrays.copyOfRange(arr, 1, arr.length);
+        int[] pArr = Arrays.copyOf(proArr, proArr.length+1);
 
+        pArr[proArr.length] = first;
+
+        result.addAll(subs(pArr, rest));
+        result.addAll(subs(proArr, rest));
+        return result;
     }
 }
