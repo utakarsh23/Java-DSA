43817808a825456f7f54e7a350a94b8e2b193ce6
utakarsh23 <utakarsh.t@somaiya.edu>
2025-12-05 19:38:31 +0530

    ach : lc - 2211  - Count Collisions On Road

--- Algorithms/LeetCode2/CoutnCollisionOnARoad.java
+++ Algorithms/LeetCode2/CoutnCollisionOnARoad.java
@@ -2,7 +2,7 @@ package LeetCode2;
 
 public class CoutnCollisionOnARoad {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println(countCollisions("RLRSLL"));
     }
 
     static public int countCollisions(String directions) {
@@ -10,13 +10,13 @@ public class CoutnCollisionOnARoad {
         int s = 0;
         int e = n;
         for(int i = 0; i < n; i++) {
-            if(directions.charAt(i) == 'L') {s++; break;}
+            if(directions.charAt(i) == 'L') s++;
+            else break;
         }
         for(int i = 0; i < n; i++) {
-            if(directions.charAt(n-1-i) == 'R') {e--; break;}
+            if(directions.charAt(n-1-i) == 'R') e--;
+            else break;
         }
-        if(s == 0 & e == n) return 0;
-
         int ans = e - s;
         for(int i = s; i < e; i++) {
             if(directions.charAt(i) == 'S') ans--;
